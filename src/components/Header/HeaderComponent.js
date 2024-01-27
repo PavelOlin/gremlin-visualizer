7e961bce65d03c96501e75d845c1492332ef2379
p.olin <p.olin@ai-mark.ru>
2024-01-28 01:00:47 +0300

    first version for builder panel

--- src/components/Header/HeaderComponent.js
+++ src/components/Header/HeaderComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, TextField }  from '@material-ui/core';
+import { Button, TextField, Select, MenuItem, FormControlLabel, Switch }  from '@material-ui/core';
 import axios from 'axios';
 import { ACTIONS, QUERY_ENDPOINT, COMMON_GREMLIN_ERROR } from '../../constants';
 import { onFetchQuery } from '../../logics/actionHelper';
@@ -11,11 +11,35 @@ class Header extends React.Component {
     this.props.dispatch({ type: ACTIONS.CLEAR_QUERY_HISTORY });
   }
 
+  buildQuery() {
+    let queryStr = this.props.query;
+    if (this.props.queryType === 'builder') {
+      queryStr = 'g.V().hasLabel("' + this.props.queryBuilderService + '")';
+      if (this.props.queryBuilderTracing) {
+        queryStr += '.emit()'
+        let directionStr = 'both()';
+        if (this.props.queryBuilderTracingDirection === 'in') {
+          directionStr = 'inE().outV()'
+        } else if (this.props.queryBuilderTracingDirection === 'out') {
+          directionStr = 'out()'
+        }
+        queryStr = queryStr + '.until(loops().is(eq(' + this.props.queryBuilderTracingSteps + '))).repeat(' + directionStr + ')'
+      }
+    }
+    return queryStr;
+  }
+
   sendQuery() {
+    this.clearGraph();
+
     this.props.dispatch({ type: ACTIONS.SET_ERROR, payload: null });
+
+    let queryStr = this.buildQuery();
+    console.info('Gremlin query = [' + queryStr + ']');
+
     axios.post(
       QUERY_ENDPOINT,
-      { host: this.props.host, port: this.props.port, query: this.props.query, nodeLimit: this.props.nodeLimit },
+      { host: this.props.host, port: this.props.port, query: queryStr, nodeLimit: this.props.nodeLimit },
       { headers: { 'Content-Type': 'application/json' } }
     ).then((response) => {
       onFetchQuery(response, this.props.query, this.props.nodeLabels, this.props.dispatch);
@@ -32,19 +56,66 @@ class Header extends React.Component {
     this.props.dispatch({ type: ACTIONS.SET_PORT, payload: port });
   }
 
+  onQueryTypeChanged(queryType) {
+    this.props.dispatch({ type: ACTIONS.SET_QUERY_TYPE, payload: queryType});
+  }
+
   onQueryChanged(query) {
     this.props.dispatch({ type: ACTIONS.SET_QUERY, payload: query });
+  }
+
+  onServiceChanged(service) {
+    this.props.dispatch({ type: ACTIONS.SET_QUERY_BUILDER_SERVICE, payload: service });
+  }
+
+  onTracingChanged(tracing) {
+    this.props.dispatch({ type: ACTIONS.SET_QUERY_BUILDER_TRACING, payload: tracing });
+  }
+
+  onTracingStepsChanged(steps) {
+    this.props.dispatch({ type: ACTIONS.SET_QUERY_BUILDER_TRACING_STEPS, payload: steps });
+  }
+
+  onTracingDirectionChanged(direction) {
+    this.props.dispatch({ type: ACTIONS.SET_QUERY_BUILDER_TRACING_DIRECTION, payload: direction });
   }
 
   render(){
     return (
       <div className={'header'}>
         <form noValidate autoComplete="off">
-          <TextField value={this.props.host} onChange={(event => this.onHostChanged(event.target.value))} id="standard-basic" label="host" style={{width: '10%'}} />
-          <TextField value={this.props.port} onChange={(event => this.onPortChanged(event.target.value))} id="standard-basic" label="port" style={{width: '10%'}} />
-          <TextField value={this.props.query} onChange={(event => this.onQueryChanged(event.target.value))} id="standard-basic" label="gremlin query" style={{width: '60%'}} />
+          <TextField value={this.props.host} onChange={(event => this.onHostChanged(event.target.value))} id="standard-basic" label="Host" style={{width: '10%'}} />
+          <TextField value={this.props.port} onChange={(event => this.onPortChanged(event.target.value))} id="standard-basic" label="Port" style={{width: '10%'}} />
+          <Select value={this.props.queryType} onChange={(event => this.onQueryTypeChanged(event.target.value))} id="standard-basic" label="Query type" style={{width: '10%'}}>
+            <MenuItem value="raw">Raw</MenuItem>
+            <MenuItem value="builder">Builder</MenuItem>
+          </Select>
+          <br />
+          <Select value={this.props.queryBuilderService} onChange={(event => this.onServiceChanged(event.target.value))} id="standard-basic" label="Direction" style={{width: '10%'}}>
+            <MenuItem value="address-service">address-service</MenuItem>
+            <MenuItem value="crpt-auth">crpt-auth</MenuItem>
+            <MenuItem value="crpt-cryto">crpt-crypto</MenuItem>
+            <MenuItem value="crpt-org-info">crpt-org-info</MenuItem>
+            <MenuItem value="elk-processor">elk-processor</MenuItem>
+            <MenuItem value="true-api">true-api</MenuItem>
+            <MenuItem value="united-auth">united-auth</MenuItem>
+          </Select>
+          <FormControlLabel
+              control={
+                <Switch checked={this.props.queryBuilderTracing} onChange={(event => this.onTracingChanged(event.target.value))} value="physics" color="primary" />
+              }
+              label="Tracing"
+          />
+          <TextField value={this.props.queryBuilderTracingSteps} onChange={(event => this.onTracingStepsChanged(event.target.value))} id="standard-basic" label="Steps" style={{width: '10%'}} />
+          <Select value={this.props.queryBuilderTracingDirection} onChange={(event => this.onTracingDirectionChanged(event.target.value))} id="standard-basic" label="Direction" style={{width: '10%'}}>
+            <MenuItem value="in">In</MenuItem>
+            <MenuItem value="out">Out</MenuItem>
+            <MenuItem value="both">Both</MenuItem>
+          </Select>
+          <br />
+          <TextField value={this.props.query} onChange={(event => this.onQueryChanged(event.target.value))} id="standard-basic" label="raw gremlin query" style={{width: '60%'}} />
           <Button variant="contained" color="primary" onClick={this.sendQuery.bind(this)} style={{width: '150px'}} >Execute</Button>
-          <Button variant="outlined" color="secondary" onClick={this.clearGraph.bind(this)} style={{width: '150px'}} >Clear Graph</Button>
+          {/*<Button variant="outlined" color="secondary" onClick={this.clearGraph.bind(this)} style={{width: '150px'}} >Clear Graph</Button>*/}
         </form>
 
         <br />
@@ -59,7 +130,12 @@ export const HeaderComponent = connect((state)=>{
   return {
     host: state.gremlin.host,
     port: state.gremlin.port,
+    queryType: state.gremlin.queryType,
     query: state.gremlin.query,
+    queryBuilderService: state.gremlin.queryBuilderService,
+    queryBuilderTracing: state.gremlin.queryBuilderTracing,
+    queryBuilderTracingSteps: state.gremlin.queryBuilderTracingSteps,
+    queryBuilderTracingDirection: state.gremlin.queryBuilderTracingDirection,
     error: state.gremlin.error,
     nodes: state.graph.nodes,
     edges: state.graph.edges,
--- src/reducers/gremlinReducer.js
+++ src/reducers/gremlinReducer.js
@@ -3,7 +3,12 @@ import { ACTIONS, SERVER_HOST } from '../constants';
 const initialState = {
   host: SERVER_HOST,
   port: '8182',
+  queryType: 'raw',
   query: 'g.V()',
+  queryBuilderService: null,
+  queryBuilderTracing: false,
+  queryBuilderTracingSteps: 1,
+  queryBuilderTracingDirection: 'out',
   error: null
 };
 
@@ -15,8 +20,23 @@ export const reducer =  (state=initialState, action)=>{
     case ACTIONS.SET_PORT: {
       return { ...state, port: action.payload }
     }
+    case ACTIONS.SET_QUERY_TYPE: {
+      return { ...state, queryType: action.payload }
+    }
     case ACTIONS.SET_QUERY: {
       return { ...state, query: action.payload, error: null }
+    }
+    case ACTIONS.SET_QUERY_BUILDER_SERVICE: {
+      return { ...state, queryBuilderService: action.payload, error: null }
+    }
+    case ACTIONS.SET_QUERY_BUILDER_TRACING: {
+      return { ...state, queryBuilderTracing: action.payload, error: null }
+    }
+    case ACTIONS.SET_QUERY_BUILDER_TRACING_STEPS: {
+      return { ...state, queryBuilderTracingSteps: action.payload, error: null }
+    }
+    case ACTIONS.SET_QUERY_BUILDER_TRACING_DIRECTION: {
+      return { ...state, queryBuilderTracingDirection: action.payload, error: null }
     }
     case ACTIONS.SET_ERROR: {
       return { ...state, error: action.payload }
